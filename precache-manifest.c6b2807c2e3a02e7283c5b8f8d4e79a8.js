1b960202d42eec9df51c6896ea7348b4365dd447
codewithrahi <rahiomkar089@gmail.com>
2020-01-29 03:10:21 +0530

    Updates

---
+++ precache-manifest.c6b2807c2e3a02e7283c5b8f8d4e79a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1127e8bc8c4bcb3efb4dc3cd0cb27218",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "bb3989adef9a76e2bfdb",
+    "url": "/Monsters-Rolodex/static/css/main.0ff258be.chunk.css"
+  },
+  {
+    "revision": "6c651e828ab46136b876",
+    "url": "/Monsters-Rolodex/static/js/2.3e911600.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.3e911600.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb3989adef9a76e2bfdb",
+    "url": "/Monsters-Rolodex/static/js/main.65954980.chunk.js"
+  },
+  {
+    "revision": "c4a876c08f90b90573c8",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.84d1eaba.js"
+  }
+]);
